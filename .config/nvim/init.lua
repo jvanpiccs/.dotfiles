e8cce4476c4a4a507bc9112a330afe29ff220053
cti23656 <juan.picco1@claro.com.ar>
2024-05-24 20:27:43 -0300

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -54,10 +54,10 @@ vim.opt.splitright = true
 vim.opt.splitbelow = true
 
 -- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
+-- See `:help 'list'`
+-- and `:help 'listchars'`
 vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+-- vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
